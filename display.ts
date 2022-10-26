d78f8039f4f4d2b3b1c41044d27f9749b9d20a85
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-10-26 14:56:29 +0800

    Update display.ts

--- display.ts
+++ display.ts
@@ -1,3 +1,7 @@
+///<reference path="_module.ts" />
+/*
+ * VelocityJS.org (C) 2014-2017 Julian Shapiro.
+ *
  * Licensed under the MIT license. See LICENSE file in the project root for details.
  */
 
